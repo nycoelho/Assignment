1437bd005c5d7d3d8d5dab58e0b2250807ab74f7
Nylson Coelho <8427999+nycoelho@users.noreply.github.com>
2024-05-11 11:04:03 +0100

    Added Country and City data configurations. Require name property with a max length of 200 characters for both Country and City.

---
+++ src/Infrastructure/Data/Configurations/CityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Assignment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Assignment.Infrastructure.Data.Configurations;
+
+public class CityConfiguration : IEntityTypeConfiguration<City>
+{
+    public void Configure(EntityTypeBuilder<City> builder)
+    {
+        builder.Property(c => c.Name)
+            .HasMaxLength(200)
+            .IsRequired();
+    }
+}
---
+++ src/Infrastructure/Data/Configurations/CountryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Assignment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Assignment.Infrastructure.Data.Configurations;
+
+public class CountryConfiguration : IEntityTypeConfiguration<Country>
+{
+    public void Configure(EntityTypeBuilder<Country> builder)
+    {
+        builder.Property(c => c.Name)
+            .HasMaxLength(200)
+            .IsRequired();
+    }
+}
