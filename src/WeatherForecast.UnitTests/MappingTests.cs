a672e2d7f1528d652ffaaf28789350c6ec036f1a
Nylson Coelho <8427999+nycoelho@users.noreply.github.com>
2024-05-11 14:55:50 +0100

    Added MappingTests. Confirm that the Dto mappings for Country and City are valid.

---
+++ src/WeatherForecast.UnitTests/MappingTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using Assignment.Application.Common.Interfaces;
+using Assignment.Application.Countries.Queries.GetCountries;
+using Assignment.Domain.Entities;
+using AutoMapper;
+
+namespace WeatherForecast.UnitTests;
+
+public class MappingTests
+{
+    private IConfigurationProvider _configurationProvider;
+    private IMapper _mapper;
+
+    [SetUp]
+    public void Setup()
+    {
+        _configurationProvider = new MapperConfiguration(config =>
+            config.AddMaps(Assembly.GetAssembly(typeof(IApplicationDbContext))));
+
+        _mapper = _configurationProvider.CreateMapper();
+    }
+
+    [Test]
+    public void IConfigurationProvider_ShouldHaveValidConfiguration()
+    {
+        // Assert
+        _configurationProvider.AssertConfigurationIsValid();
+    }
+
+    [Test]
+    [TestCase(typeof(Country), typeof(CountryDto))]
+    [TestCase(typeof(City), typeof(CityDto))]
+    public void IMapper_ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+    {
+        // Arrange
+        var instance = GetInstanceOf(source);
+
+        // Act
+        _mapper.Map(instance, source, destination);
+    }
+
+    private object GetInstanceOf(Type type)
+    {
+        if (type.GetConstructor(Type.EmptyTypes) is not null)
+        {
+            return Activator.CreateInstance(type)!;
+        }
+
+        // Type without parameterless constructor
+        return RuntimeHelpers.GetUninitializedObject(type);
+    }
+}
