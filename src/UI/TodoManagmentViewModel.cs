2ea0fd48352c63acf30b999a2c0a621dd8d48f6a
Nylson Coelho <8427999+nycoelho@users.noreply.github.com>
2024-05-11 10:27:55 +0100

    Updated user interface when adding Todo Lists, Todo Items or setting Done in Todo Items.

--- src/UI/TodoManagmentViewModel.cs
+++ src/UI/TodoManagmentViewModel.cs
@@ -81,17 +81,26 @@ internal class TodoManagmentViewModel : Screen
     private async void AddTodoList(object obj)
     {
         var todoList = new TodoListViewModel(_sender);
-        await _windowManager.ShowDialogAsync(todoList);
+        bool? result = await _windowManager.ShowDialogAsync(todoList);
+        if (result == true)
+        {
+            await RefreshTodoLists();
+        }
     }
 
     private async void AddTodoItem(object obj)
     {
         var todoItem = new TodoItemViewModel(_sender, SelectedTodoList.Id);
-        await _windowManager.ShowDialogAsync(todoItem);
+        bool? result = await _windowManager.ShowDialogAsync(todoItem);
+        if (result == true)
+        {
+            await RefreshTodoLists();
+        }
     }
 
     private async void DoneTodoItem(object obj)
     {
         await _sender.Send(new DoneTodoItemCommand(SelectedItem.Id));
+        await RefreshTodoLists();
     }
 }
