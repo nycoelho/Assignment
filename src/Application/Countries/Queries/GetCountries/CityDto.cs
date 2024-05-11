9f2b6dbb24cafbaa6f7f04d5a7daeb7c79479d08
Nylson Coelho <8427999+nycoelho@users.noreply.github.com>
2024-05-11 11:32:28 +0100

    Added data transfer objects for Country and City.

---
+++ src/Application/Countries/Queries/GetCountries/CityDto.cs
@@ -0,0 +1,20 @@
+﻿using Assignment.Domain.Entities;
+
+namespace Assignment.Application.Countries.Queries.GetCountries;
+
+public class CityDto
+{
+    public int Id { get; init; }
+
+    public int CountryId { get; init; }
+
+    public string? Name { get; init; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<City, CityDto>();
+        }
+    }
+}
---
+++ src/Application/Countries/Queries/GetCountries/CountryDto.cs
@@ -0,0 +1,25 @@
+﻿using Assignment.Domain.Entities;
+
+namespace Assignment.Application.Countries.Queries.GetCountries;
+
+public class CountryDto
+{
+    public CountryDto()
+    {
+        Cities = [];
+    }
+
+    public int Id { get; init; }
+
+    public string? Name { get; init; }
+
+    public IList<CityDto> Cities { get; init; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Country, CountryDto>();
+        }
+    }
+}
