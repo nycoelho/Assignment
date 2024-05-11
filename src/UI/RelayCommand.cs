4ca07f37e1b041fb88a0379b32509728b61f7e5c
Nylson Coelho <8427999+nycoelho@users.noreply.github.com>
2024-05-11 10:40:35 +0100

    Implement missing CanExecute predicate and logic in RelayCommand. Add constructor for RelayCommands with CanExecute predicate.

--- src/UI/RelayCommand.cs
+++ src/UI/RelayCommand.cs
@@ -4,13 +4,19 @@ namespace Assignment.UI;
 public class RelayCommand : ICommand
 {
     private readonly Action<object> _execute;
+    private readonly Predicate<object> _canExecute;
 
-    public RelayCommand(Action<object> execute)
+    public RelayCommand(Action<object> execute) : this(execute, null)
     {
-        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
     }
 
-    public bool CanExecute(object parameter) => true;
+    public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter) => _canExecute is null || _canExecute(parameter);
 
     public void Execute(object parameter) => _execute(parameter);
 
