e8974d0bd53dde9ea25ec5e943c2363e868de2b0
Nylson Coelho <8427999+nycoelho@users.noreply.github.com>
2024-05-11 11:35:59 +0100

    Added GetCountries query and query handler.

---
+++ src/Application/Countries/Queries/GetCountries/GetCountries.cs
@@ -0,0 +1,28 @@
+﻿using Assignment.Application.Common.Interfaces;
+using Assignment.Application.Common.Security;
+
+namespace Assignment.Application.Countries.Queries.GetCountries;
+
+[Authorize]
+public record GetCountriesQuery : IRequest<IList<CountryDto>>;
+
+public class GetCountriesQueryHandler : IRequestHandler<GetCountriesQuery, IList<CountryDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetCountriesQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<IList<CountryDto>> Handle(GetCountriesQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Countries
+            .AsNoTracking()
+            .ProjectTo<CountryDto>(_mapper.ConfigurationProvider)
+            .OrderBy(c => c.Name)
+            .ToListAsync(cancellationToken);
+    }
+}
