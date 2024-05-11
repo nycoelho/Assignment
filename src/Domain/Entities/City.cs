cc27717164717ba3e573022b1ee45c383a04be6b
Nylson Coelho <8427999+nycoelho@users.noreply.github.com>
2024-05-11 10:54:33 +0100

    Add two new entities to Domain, Country and City, with a one-to-many relationship.

---
+++ src/Domain/Entities/City.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment.Domain.Entities;
+
+public class City : BaseAuditableEntity
+{
+    public int CountryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public Country Country { get; set; } = default!;
+}
---
+++ src/Domain/Entities/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace Assignment.Domain.Entities;
+
+public class Country : BaseAuditableEntity
+{
+    public string? Name { get; set; }
+
+    public IList<City> Cities { get; private set; } = [];
+}
